fb50ded6b9dbd3e93abb8bf88d5f442f9d701c39
Ganesh Dasari <gdasari74@gmail.com>
2019-11-10 00:04:31 +0530

    updated

--- mongodbcurd/index.js
+++ mongodbcurd/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const MongoClient = require("mongodb").MongoClient;
+var exphbs = require("express-handlebars");
 const PORT = 9090;
 
 app.use(express.json());
 app.use(express.urlencoded());
 app.use("/public", express.static("public"));
+
+//Handlebar configuration
+app.engine(".hbs", exphbs({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 //Database connection
 var db = null;
@@ -15,39 +20,35 @@ MongoClient.connect(url, function(error, client) {
   if (error) {
     throw error;
   }
-  db = client.db("eagle-db");
-  collection = db.collection("students");
+  db = client.db("eagle-ganesh-dasari");
+  collection = db.collection("users");
+});
+app.get("/insertpage", function(req, res) {
+  return res.render("insertpage");
 });
 
 app.post("/insert", function(req, res) {
   var data = req.body;
   collection.insertOne(data, function(error, data) {
     if (error) {
-      return res.status(500).json({
-        status: false,
-        message: "Falied to insert"
-      });
+      return res.status(500).send("<h1>Data Insertion Failed</h1>");
     }
-    return res.status(200).json({
-      status: true,
-      message: "Insert Successful"
-    });
+    return res.redirect("/");
   });
 });
 
-app.get("/retrive", function(req, res) {
-  var collection = db.collection("students");
+app.get("/", function(req, res) {
   collection.find({}).toArray(function(error, data) {
     if (error) {
-      return res.status(500).json({
+      return res.render("home", {
         status: false,
-        message: "Falied to insert"
+        message: "Falied to get the list! "
       });
     }
 
-    return res.status(200).json({
+    return res.render("home", {
       status: true,
-      message: data
+      data: data
     });
   });
 });
